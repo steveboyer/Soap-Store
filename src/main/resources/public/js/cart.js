dc18b13954a62d70e0f9e1c5532b230c1de8a455
Steve Boyer <sboyer307@gmail.com>
2018-03-13 13:58:53 -0600

    Added js, images, css

---
+++ src/main/resources/public/js/cart.js
@@ -0,0 +1,35 @@
+$(function(){
+    console.log("Page loaded");
+    $("#cart-button-header").collapse("hide");
+
+    $("#add_button").click(function() {
+        console.log("clicked");
+        var quantity = $("select#sel-quantity").val();
+        var scent = $("select#sel-scent").val();
+        var size = $("select#sel-size").val();
+        console.log(quantity + " " + scent + " " + size);
+        var productId = "KKJJEKD";
+        var data = {
+            "productId": productId,
+            "quantity": quantity,
+            "scent": scent,
+            "size": size
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/store/item/add",
+            dataType: "json",
+            contentType: "application/json",
+            success: function(response){
+                console.log(response);
+            },
+            data: JSON.stringify(data)
+        }).done(function(response){
+            console.log(response);
+            $("#cart-button-header").collapse("show");
+        });
+
+    });
+});
+
