72be6875381fe8866107b9d154aa5033d0a32386
Steve Boyer <sboyer307@gmail.com>
2018-04-29 21:39:38 -0600

    Finishing touches

---
+++ src/main/java/com/stephenboyer.soapstore/util/ProductComparator.java
@@ -0,0 +1,13 @@
+package com.stephenboyer.soapstore.util;
+
+import com.stephenboyer.soapstore.domain.Catalog;
+
+import java.util.Comparator;
+
+public class ProductComparator implements Comparator<Catalog> {
+
+    @Override
+    public int compare(Catalog o1, Catalog o2) {
+        return 0;
+    }
+}
